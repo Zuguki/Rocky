cabe712b69807cb8ca73e41a66ca8d8fd80ea092
Zuguki <guskov.zahar20@gmail.com>
2021-12-02 13:08:19 -0500

    DetailsVM done

--- Models/ViewModels/DetailsVM.cs
+++ Models/ViewModels/DetailsVM.cs
@@ -2,6 +2,12 @@ namespace Rocky.Models.ViewModels
 {
     public class DetailsVM
     {
+        public DetailsVM()
+        {
+            Product = new Product();
+        }
         
+        public Product Product { get; set; }
+        public bool ExistsInCart { get; set; }
     }
 }
