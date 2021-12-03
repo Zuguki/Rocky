5f0676a999c6160aaf9deae1d6375787f952beec
Zuguki <guskov.zahar20@gmail.com>
2021-12-03 15:22:58 -0500

    WC added Session Cart

--- WC.cs
+++ WC.cs
@@ -3,5 +3,6 @@ namespace Rocky
     public static class WC
     {
         public static string ImagePath { get; } = @"/images/product/";
+        public static string SessionCart = @"ShoppingCartSession";
     }
 }
